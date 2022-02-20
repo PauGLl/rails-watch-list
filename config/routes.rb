2948e9b3653dc063776999b679b9dbb37c48fb84
Paula Garcia Llopis <paulagllopis@gmail.com>
2022-02-20 17:31:34 +0100

    adding the root_path route

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'lists#index'
   resources :lists do
-    resources :bookmarks, only: [:new, :create, :destroy]
+    resources :bookmarks, only: [ :new, :create, :destroy ]
   end
   resources :lists do
-    resources :reviews, only: [:new, :create]
+    resources :reviews, only: [ :new, :create ]
   end
 end
